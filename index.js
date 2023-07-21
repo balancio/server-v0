b90fe17bca3da54b2d4a304a5e69b08720ae61b8
Dragor Draganović <dragor.draganovic@gmail.com>
2023-07-21 16:14:30 +0200

    feat(server): implement base express web server

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+import { config as envConfig } from 'dotenv'
+import express from 'express'
 import { database } from './services/database'
 
-const data = []
+envConfig()
 
-const result = await database.run(
-    async (db) => {
-        const coll = db.collection("users")
-        const cursor = coll.find()
-        for (let doc = await cursor.tryNext(); doc != null; doc = await cursor.tryNext()) {
-            console.log(doc.username)
-            data.push(doc.username)
+// --- Setup Express Application ---
+
+const srv = {
+    PORT: Number(process.env.SRV_PORT),
+    HOST: process.env.SRV_HOST,
+}
+
+console.log(srv)
+
+const app = express()
+
+app.listen(srv.PORT, srv.HOST, async () => {
+    console.log(`Listening on port ${srv.PORT}`)
+    // Test db conn
+    await database.run(
+        async (db) => {
+            const coll = db.collection("users")
+            const cursor = coll.find()
+            for (let doc = await cursor.tryNext(); doc != null; doc = await cursor.tryNext()) {
+                console.log(doc.username)
+            }
         }
-    }
-)
-
-console.log(data)
+    )
+})
